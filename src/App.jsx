ca9778a760fc80ff6fbd7c74e87c06f806c4289b
Lean <cxclx@outlook.com>
2023-11-10 10:34:59 +0100

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
+
+import Home from "./pages/home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/*<Route path="*" element={<NotFound />} />*/}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
