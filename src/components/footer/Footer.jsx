0f5c50df3e1ee9858570720ff4abdd0754891cfe
Lean <cxclx@outlook.com>
2023-11-13 06:11:34 +0100

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,17 @@
 import styles from "./Footer.module.scss";
-import Diconium from "../../assets/img/diconium-white.png";
+import Diconium from "../../assets/img/dic.png";
 
 function Footer() {
   return (
     <footer className={styles}>
+      <div className={styles.footer__menu}>
       <img src={Diconium} alt="diconium digial solutions"></img>
       <ul>
-        <li></li>
+        <li>Imprint</li>
+        <li>About</li>
       </ul>
+      </div>
+      <span>©2023 diconium GmbH</span>
     </footer>
   );
 }
