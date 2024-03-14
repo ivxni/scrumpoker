01c28117554f712f313bf76569a9117faaf7b88e
ivxni <ivxnv@outlook.com>
2024-03-14 20:01:50 +0100

    Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -17,7 +17,35 @@ function Home() {
             <Button variant={"primary"} label={"Create Room"} />
           </Link>
         </div>
-        <div className={styles.video}></div>
+        <div className={styles.video}>
+          <div className={styles.row}>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.card}></div>
+            <div className={styles.table_container}>
+              <div className={styles.table}>Reveal Cards</div>
+            </div>
+            <div className={styles.card}></div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+            <div className={styles.card}></div>
+          </div>
+          <div className={styles.row}>
+            <div className={styles.card_fibo}>1</div>
+            <div className={styles.card_fibo}>2</div>
+            <div className={styles.card_fiboSelected}>3</div>
+            <div className={styles.card_fibo}>5</div>
+            <div className={styles.card_fibo}>8</div>
+            <div className={styles.card_fibo}>13</div>
+            <div className={styles.card_fibo}>?</div>
+            <div className={styles.card_fibo}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
